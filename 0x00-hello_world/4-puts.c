5d9fb3f79a8e02359292c134318f398bd48ffec1
Shikanda <angareshikanda@gmail.com>
2022-12-08 11:34:10 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description : Hello puts
+*Return: Always 0 (Success )
+*/
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
