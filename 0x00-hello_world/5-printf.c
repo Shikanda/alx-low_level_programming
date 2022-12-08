1f7617709e611b48ac83cb5709e09e3d1d6cb6b8
Shikanda <angareshikanda@gmail.com>
2022-12-08 12:03:34 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - hello Printf
+*Description: print a sentence
+*Return: success 0
+*/
+int main(void)
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
