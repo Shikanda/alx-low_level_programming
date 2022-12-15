d827dfbf0485ffc6c6bae35a7139aa582ad9014c
Shikanda <angareshikanda@gmail.com>
2022-12-15 12:11:47 -0800

    The shorteset distance between two points is a a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_line - draw a line in the terminal
+*@n: times to print underscore
+*
+*Return: line
+*/
+
+void print_line(int n)
+{
+	int line;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		line = 0;
+		while (line <= n)
+		{
+			_putchar('_');
+			line++;
+		}
+		_putchar('\n');
+	}
+}
