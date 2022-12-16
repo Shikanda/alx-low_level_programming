7eb2cab117ecd7c0ba6cd644cf6b2734e33e50f4
Shikanda <angareshikanda@gmail.com>
2022-12-16 11:38:22 -0800

    prime  number that works

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,13 +1,33 @@
 #include <stdlib.h>
+#include <stdio.h>
 /**
-*
-*main - entry point
+*main - prime factor of 612852475143
 *
 *
 *Return: always 0
 */
 
-int main(void)]
+int main(void)
 {
+	long prime = 612852475143;
+	long i;
+
+	while (i < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+
+		for (i = 3; i < (prime / 2); i += 2)
+		{
+			if ((prime % i) == 0)
+				prime /= i;
+		}
+	}
+
+	printf("%ld\n", prime);
+
 	return (0);
 }
