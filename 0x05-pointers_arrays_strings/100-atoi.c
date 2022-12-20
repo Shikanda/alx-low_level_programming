af325a6d4b04d326aef54e07dfbdaa6bb8b0105d
Shikanda <angareshikanda@gmail.com>
2022-12-20 14:56:59 -0800

    task 10 correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,8 +9,23 @@
 
 int _atoi(char *s)
 {
+	int atoi;
+	unsigned int i;
 
+	atoi = 1;
+	i = 0;
 
-	return (int);
+	do {
+		if (*s == '-')
+			atoi *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			i = (i * 10) + (*s - '0');
+
+		else if (i > 0)
+			break;
+
+	} while (*s++);
+
+	return (i * atoi);
 }
-
