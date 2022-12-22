5972b60138d51c68ba23812e06fdbc79476d518f
Shikanda <angareshikanda@gmail.com>
2022-12-22 01:29:24 -0800

    stringtoupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,15 +3,14 @@
 *string_toupper - change lowercase to uppercase
 *@str: the strings to be changed
 *
-*Return: always 0
+*Return: pointer to string
 */
 char *string_toupper(char *str)
 {
 	int i;
 
 	i = 0;
-
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if (str[i] > 'a' && str[i] <= 'z')
 			str[i] -= 32;
