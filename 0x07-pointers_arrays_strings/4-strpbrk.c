9b3820f3632250fd71b396140db4b8a50da70d47
Shikanda <angareshikanda@gmail.com>
2023-01-03 11:42:55 -0800

    crackme password

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
+*_strpbrk - search a string fofr any of a set of bytes
+*@s: string
+*@accept a string which ha s the set of bytes
 *
-*
-*
-*
-*
+*Return: byte in s
 */
-
+char *_strpbrk(char *s, char *accept)
+{
+	
