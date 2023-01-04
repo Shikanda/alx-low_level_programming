2182b35047ed191b916863da5bf4a953d592bb0d
Shikanda <angareshikanda@gmail.com>
2023-01-04 14:10:52 -0800

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsum.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_diagsums - print the sum of two daigonals
+*@a: first array
+*@size: second array
+*
+*Return: Always 0
+*/
+void print_diagsums(int *a, int size)
+{
+
+	Return(0);
+}
