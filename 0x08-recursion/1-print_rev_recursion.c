e27c7677a17bb6d1b8a0cceaa581763352dcff30
Shikanda <angareshikanda@gmail.com>
2023-01-05 02:57:35 -0800

    string reversed

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,18 @@
 #include "main.h"
+
 /**
+*_print_rev_recursion - reverse prints a string
+*@s: string in question
 *
-*
-*
-*
-*
+*Return: always 0
 */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
