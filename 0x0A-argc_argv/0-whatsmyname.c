3291011ffbbe1dae585b174e919fc14f9cada783
Shikanda <angareshikanda@gmail.com>
2023-01-09 10:32:32 -0800

    change 1st element name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - entry point
+*@argc: number of arrays
+*@argv: array of strings
+*Return: On success 1.
+*
+*/
+int  main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
