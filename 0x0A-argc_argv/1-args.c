31b69e6fed6c48cdd88f7c0126c857ea2f9d80f7
Shikanda <angareshikanda@gmail.com>
2023-01-09 11:49:53 -0800

    number of arguements passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - entry point
+*@argc: number of arrays
+*@argv: array of strings
+*Return: On success 1.
+*
+*/
+int  main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
