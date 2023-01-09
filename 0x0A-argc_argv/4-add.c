05c60512c5f9575fc392f8d8da5ce4d45b212054
Shikanda <angareshikanda@gmail.com>
2023-01-09 14:43:40 -0800

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - entry point
+*@argc: number of arrays
+*@argv: array of strings
+*Return: On success 0 else 1 with Error.
+*atoi - converts string into an integer
+*/
+int  main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	if (argc < 1)
+	{
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
