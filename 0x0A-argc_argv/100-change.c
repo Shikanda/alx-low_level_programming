9f7f933a455b363506913bb831178a4fae22b954
Shikanda <angareshikanda@gmail.com>
2023-01-09 15:27:13 -0800

    +ve integer addition

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,23 +9,4 @@
 */
 int  main(int argc, char *argv[])
 {
-	int i;
-	int add;
-
-	add = 0;
-
-	if (argc < 1)
-		return (0);
-
-	for (i = 1; i < argc; i++)
-	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
-		add = add + atoi(argv[i]);
-	}
-	printf("%d\n", add);
-	return (0);
 }
