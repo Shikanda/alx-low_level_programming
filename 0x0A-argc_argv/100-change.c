b90323d991cebc4e612ee1904ee6bbc03c755235
Shikanda <angareshikanda@gmail.com>
2023-01-10 08:18:18 -0800

    change coin problem

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,4 +9,44 @@
 */
 int  main(int argc, char *argv[])
 {
+	int coins;
+	int cents;
+
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else if (argc < 0)
+	{
+		return (0);
+	}
+	coins = atoi(argv[1]);
+	cents = 0;
+	while (cents - 25)
+	{
+		coins++;
+		while (cents >= 25)
+		{
+			cents = cents - 25;
+		}
+		while (cents >= 10)
+		{
+			cents = cents - 10;
+		}
+		while  (cents >= 5)
+		{
+			cents = cents - 5;
+		}
+		while (cents >= 2)
+		{
+			cents = cents - 2;
+		}
+		while (cents >= 1)
+		{
+			cents = cents - 1;
+		}
+		coins--;
+	}
+	return (coins);
 }
