e4390750d3861420fe99ea552294e60fe94c3da8
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-11 22:40:58 +0300

    malloc to 2 dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*alloc_grid -2d array of integers to malloc allocated memory
+*@width: size
+*@height: array size
+*Return: NULL if size is 0 or pointer to the array
+*/
+int **alloc_grid(int width, int height)
+{
+	int h;
+	int w;
+	int **arr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = (int **)malloc(width * sizeof(int *));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (w = 0; w < width; w++)
+	{
+		arr[w] = (int *)malloc(height * sizeof(int));
+		if (arr[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(arr[w]);
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (w = 0; w  < width; w++)
+	{
+		for (h = 0; h < width; h++)
+			arr[w][h] = 0;
+	}
+	return (arr);
+}
