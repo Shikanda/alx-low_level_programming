995a7d5a89dba18e0362d3c0fde8301865c96247
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-12 22:29:39 +0300

    malloc to return a pointer to a 2D array

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*strtow - a pointer to an array of strings
+*@str: array of strings
+*Return: NULL or pointer to a new string
+*/
+char **strtow(char *str)
+{
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	return (strtow);
+}
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,11 +34,12 @@ int **alloc_grid(int width, int height)
 			free(arr);
 			return (NULL);
 		}
-	}
-	for (w = 0; w  < width; w++)
-	{
+
+		for (w = 0; w  < width; w++)
+		{
 		for (h = 0; h < height; h++)
 			arr[w][h] = 0;
+		}
 	}
 	return (arr);
 }
