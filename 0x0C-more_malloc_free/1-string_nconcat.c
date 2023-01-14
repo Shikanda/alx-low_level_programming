7c388b71276a67574a66c251238ab6f32e2cec1e
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-15 02:02:55 +0300

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
-*string_noncat - concatecates 2 strings
+*string_nconcat - concatecates 2 strings
 *@s1: first  string
 *@s2: second string
 *@n: number of bytes
@@ -13,38 +13,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
 	unsigned int i;
-	unsigned int j;
 	unsigned int st1;
-	unsigned int st2;
+
+	st1 = n;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i]; i++)
 
-	for (j = 0; s2[j] != '\0'; j++)
+	{
+		st1++;
+	}
 
-	if (n < j)
-		j = n;
-
-	j = j + i;
-
-	str = malloc(sizeof(char *) * (j + 1));
+	str = malloc(sizeof(char) * (st1 + 1));
 
 	if (str == NULL)
 		return (NULL);
 
-	for (st1 = 0; st1 < i; st1++)
+	for (i = 0; s1[i]; i++)
 		str[st1] = s1[st1];
 
-	for (st2 = 0; st2 < j; st2++)
+	for (i = 0; s2[i]  && i < n; i++)
 	{
-		str[st1] = s2[st2];
+		str[st1++] = s2[i];
 		st1++;
 	}
-	st1++;
 	str[st1] = '\0';
 	return (str);
 }
