8e113db833a2f4c9c0b8870bc373c90e98046b80
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-18 22:14:09 +0300

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,12 +10,44 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	typedef struct dog
+	dog_t *new;
+	char *cpname;
+	char *cpowner;
+	unsigned int i;
+	unsigned int nalen;
+	unsigned int ownlen;
+
+	nalen = 0;
+	ownlen = 0;
+	new = malloc(sizeof(dog_t));
+	if (name == NULL)
+		return (NULL);
+	if (name == NULL || age <= 0 || owner == NULL)
 	{
-		char *name;
-		float age;
-		char *owner;
-	} new_dog;
+		free(new);
+		return (NULL);
+	}
 
+	for (i = 0; name[i] != '\0'; i++)
+		nalen++;
 
+	for (i = 0; owner[i] != '\0'; i++)
+		ownlen++;
+
+	cpname = malloc(sizeof(char) * (nalen + 1));
+		if (cpname == NULL)
+			return (NULL);
+	cpowner = malloc(sizeof(char) * (ownlen + 1));
+		if (cpowner == NULL)
+			return (NULL);
+
+	for (i = 0; i <= nalen; i++)
+		cpname[i] = name[i];
+	for (i = 0; i <= ownlen; i++)
+		cpowner[i] = owner[i];
+
+	new->name = cpname;
+	new->owner = cpowner;
+	new->age = age;
+	return (new);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -12,7 +12,5 @@ void free_dog(dog_t *d);
 	if (d == NULL)
 		return;
 
-	free((*d).owner);
-	free((*d).name);
 	free(d);
 }
