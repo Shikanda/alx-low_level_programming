42dfe65055e2c4792e1c731b0d1fe6a65d6def21
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-18 03:03:53 +0300

    free dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,14 +10,21 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if ((*d).name == NULL)
+	if (d->name == NULL)
 		 printf("Name: (nil)\n");
-	if ((*d).age <= 0)
-		 printf("Age: (nil)\n");
-	if ((*d).owner == NULL)
-		 printf("Owner: (nil)\n");
+	else
+		printf("Name:  %s\n", d->name);
 
-	printf("Name:  %s\n", d->name);
-	printf("Age:  %6f\n", d->age);
-	printf("Owner:  %s\n", d->owner);
+
+	if (d->age <= 0)
+		 printf("Age: (nil)\n");
+	else
+		printf("Age:  %6f\n", d->age);
+
+
+	if (d->owner == NULL)
+		 printf("Owner: (nil)\n");
+	else
+		printf("Owner:  %s\n", d->owner);
+
 }
