75fe1fbdeeba2f4dfa16b4d03715b85fff25944e
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-20 22:59:36 +0300

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*print_-all - print a list of types of arguments followed by a new line.
+*@format: the arguments
+*Return: if string is NULL print nil
+*/
+void print_all(const char * const format, ...);
+
+{
+	va_list args;
+	int i;
+
+
+	}
+
+	printf("\n");
+	va_end(args);
+}
