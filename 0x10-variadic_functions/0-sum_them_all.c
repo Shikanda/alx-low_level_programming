56ea4e5b8a1628e673cffcef091b1ae854ef3a14
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-20 19:46:10 +0300

    add em up

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*sum_them_all - sum all the parameters.
+*@n: count of remaining arguments
+*
+*Return: 0 if n is NULL
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	va_list add_em;
+	unsigned int i;
+	unsigned int add;
+
+	va_start(add_em, n);
+
+	add = 0;
+	for ( i = 0; i < n; i++)
+	{
+		add = add + va_arg(add_em, int);
+	}
+	va_end (add_em);
+	return (add_em);
+}
