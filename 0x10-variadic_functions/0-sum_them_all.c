a506a255ca38ef52e3eed3851d1ea5cbc2768bcd
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-20 19:56:40 +0300

    sum of all parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,20 +12,20 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
-
 	va_list add_em;
 	unsigned int i;
 	unsigned int add;
+
+	if (n == 0)
+		return (0);
 
 	va_start(add_em, n);
 
 	add = 0;
 	for ( i = 0; i < n; i++)
 	{
-		add = add + va_arg(add_em, int);
+		add = add + va_arg(add_em, unsigned int);
 	}
 	va_end (add_em);
-	return (add_em);
+	return (add);
 }
