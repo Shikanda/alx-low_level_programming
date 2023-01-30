3bf87b7f1a344b5ea1a7768a35869da2bd76f3a6
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-30 17:45:06 +0300

    __attribute__((constructor)) tortoise won

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+*tortoise - print tortoise.
+*/
+void __attribute__((constructor)) tortoise()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
