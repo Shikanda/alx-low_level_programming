7fed596ef9f32d42581c03d4c8857d11976966b1
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-02-06 16:13:59 +0300

    set value of bit at given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*set_bit - sets value of a bit at a given index
+*@n: decimal number with the bit
+*@index: index starting from 0 of the bit i want
+*Return: 1 if it worked or -1 incase of an error
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (8 * sizeof(unsigned long int)))
+		return (-1);
+
+	*n &= ~(1 << index);
+	return (1);
+}
---
+++ 0x14-bit_manipulation/sizeofalt.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include<stdio.h>
+
+#define sizes (8 * (unsigned long int)sizeof(siz))
+int alt_size(unsigned long int siz)
+{
+	return (sizes(siz));
+}
