ace49012ad3968f44cf345328acf1f6c557d783b
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-02-08 15:18:53 +0300

    error correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <sys/types.h>
+#include <sys/stat.h>
 /**
 *read_textfile -reads a text file and prints it to the POSIX stdout
 *@filename: stream of the file to be read
@@ -11,38 +12,39 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fname;
 	char *buff;
-	ssize_t rbyte;
-	ssize_t wbyte;
+	int rbyte;
+	int wbyte;
 
-	if (!filename)
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL || filename == NULL)
 		return (0);
 
 
 	fname = open(filename, O_RDONLY);
 
-	if (fname == -1)
-		return (0);
-
-	buff = malloc(letters);
-
-	if (!buff)
-	return (0);
-
-	rbyte = read(fname, buff, letters);
-	if (rbyte == -1)
+	if (fname < 0)
 	{
 		free(buff);
 		return (0);
 	}
 
+	rbyte = read(fname, buff, letters);
+	if (rbyte < 0)
+	{
+		close(fname);
+		free(buff);
+		return (0);
+	}
 	wbyte = write(STDOUT_FILENO, buff, rbyte);
 
-	if (wbyte == -1 || wbyte != rbyte)
+	if (wbyte < 0 || wbyte != rbyte)
 	{
+		close(fname);
 		free(buff);
 		return (0);
 	}
 
 	free(buff);
+	close(fname);
 	return (rbyte);
 }
